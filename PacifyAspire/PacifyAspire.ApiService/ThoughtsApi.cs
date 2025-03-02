55478641bf1f94b3b80addd20e45cd59d0cd8e76
ameysunu <ameysunu00719@gmail.com>
2025-03-02 16:07:09 +0000

    backend api configured

---
+++ PacifyAspire/PacifyAspire.ApiService/ThoughtsApi.cs
@@ -0,0 +1,18 @@
+﻿namespace PacifyAspire.ApiService
+{
+    public class ThoughtsApi
+    {
+        private readonly HttpClient _httpClient;
+
+        public ThoughtsApi(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetDataAsync()
+        {
+            return await _httpClient.GetStringAsync("http://localhost:7128/api/GetThoughtsImages");
+        }
+    }
+
+}
