9ebb64bef8fd453feeb39e20de44fc80065934eb
ameysunu <ameysunu00719@gmail.com>
2025-03-17 14:00:54 +0000

    stats tied up in frontend and webservice

---
+++ PacifyAspire/PacifyAspire.Web/Components/Models/StatsModel.cs
@@ -0,0 +1,17 @@
+﻿namespace PacifyAspire.Web.Components.Models
+{
+    public class StatsModels
+    {
+        public string frequentMoodIntensity { get; set; }
+        public string frequentMood { get; set; }
+        public Dictionary<string, double> moodIntensityDistribution { get; set; }
+        public double moodIntensityVariance { get; set; }
+        public List<string> dailyMoodChanges { get; set; }
+        public Dictionary<string, string> commonMoodsPerTimeframe { get; set; }
+        public Dictionary<string, double> longestMoodStreak { get; set; }
+        public List<string> suddenMoodShift { get; set; }
+        public List<string> unusualHighIntensityMoodPattern { get; set; }
+        public string userId { get; set; }
+        public string id { get; set; }
+    }
+}
